9bc7baba95c770f77ec4b03c6704cd49cf0ee53a
Gatowo777 <zamoraaldana6@gmail.com>
2025-09-19 20:35:01 -0500

    3006

--- bd.py
+++ bd.py
@@ -3,7 +3,7 @@ import pymysql
 
 def obtener_conexion():
     return pymysql.connect(host='localhost',
-                                port=3327,
+                                port=3306,
                                 user='root',
                                 password='',
                                 db='brain_rush')
