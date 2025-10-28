d2a2db5858c8a8dda54404f75f619ecd263ba5a6
IgnacioAlP <alonzopezoi@gmail.com>
2025-10-28 12:46:15 -0500

    Revert "responso y cambuio boton"
    This reverts commit 2938a00d57d39c92269576cf975f5289e682061f.

--- bd.py
+++ bd.py
@@ -45,7 +45,7 @@ def inicializar_usuarios_prueba():
 
 def obtener_conexion():
     return pymysql.connect(host='localhost',
-                                port=3327,
+                                port=3306,
                                 user='root',
                                 password='',
                                 db='brain_rush')
